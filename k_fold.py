56c5196c4b9d14e6345a91ca5c7995b88e8c9f10
Riko <riko@riko.fi>
2017-12-05 13:06:48 +0200

    Creating confusion Matrixes from K-fold validation

--- k_fold.py
+++ k_fold.py
@@ -1,10 +1,29 @@
 from sklearn import linear_model
 import k_fold_data_log_loss
 from sklearn.model_selection import KFold
-import csv
+import pandas as pd
+from pandas_ml import ConfusionMatrix
+import matplotlib.pyplot as plt
 
 # TODO: Documentation on what is done
 
+
+def save_k_fold_results_to_CSV(csv_file):
+    # Save results to k_fold_validation.csv file
+    csv_file = pd.DataFrame(
+        csv_file, columns=['Training or Validation data', 'Accuracity', 'Sample size', 'Wrong labels', 'Used samples(test set)'])
+    csv_file.to_csv('predictions/k_fold_validation.csv',
+                            index=False, header=True, sep=',')
+    print('K-fold results saved to predictions/k_fold_validation.csv file')
+
+def plot_confusion_matrix(clf, X_best_test, y_best_test, matrix_name):
+    # Doing the confusion matrix for the best K-validated training set
+    y_best_pred = clf.predict(X_best_test)
+    confusion_matrix = ConfusionMatrix(y_best_test, y_best_pred)
+    print("Confusion matrix for the best K-fold validated training set:\n{}".format(confusion_matrix))
+    confusion_matrix.plot(normalized=True)
+    plt.savefig('confusion_matrixes/K-fold_matrix_{}.png'.format(matrix_name))
+    print('Saved Confusion matrix to confusion_matrixes/K-fold_matrix_{}.png'.format(matrix_name))
 
 def run(train_bunch, test_bunch):
     print('Running K-fold...')
@@ -16,12 +35,12 @@ def run(train_bunch, test_bunch):
     csv_file = [['Training or Validation data', 'Accuracity',
                  'Sample size', 'Wrong labels', 'Used samples (test set)']]
     best_accuracy = 0
+    matrix_name = 1
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
         clf = linear_model.LogisticRegression()
-        #clf = linear_model.LinearRegression()
         clf.fit(X_train, y_train)
 
         y_pred = clf.predict(X_test)
@@ -42,24 +61,25 @@ def run(train_bunch, test_bunch):
         csv_file.append(['TRAIN', accuracity, len(
             y_train), wrong_labels, list(train_index)])
 
+        plot_confusion_matrix(clf, X_test, y_test, matrix_name)
+        matrix_name += 1 
         if best_accuracy < test_accuracity:
             best_accuracy = test_accuracity
             X_best_train, X_best_test = X[train_index], X[test_index]
             y_best_train, y_best_test = y[train_index], y[test_index]
+        
 
-    # Save results to k_fold_validation.csv file
-    with open("predictions/k_fold_validation.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(csv_file)
-    print('Results saved to k_fold_validation.csv file')
+    save_k_fold_results_to_CSV(csv_file)
 
     # Doing the classification with the best k-fold data
     print('Best test accuracity if {} with train dataset of {} datapoint'.format(
         best_accuracy, len(y_best_train)))
 
-    print('Calculating the classifier with the best training set...')
+    print('Calculating the classifier with the best K-fold validated training set...')
     clf = linear_model.LogisticRegression()
     clf.fit(X_best_train, y_best_train)
+    matrix_name = 'best_accuracity_({})'.format(best_accuracy)
+    plot_confusion_matrix(clf, X_best_test, y_best_test, matrix_name)
 
     k_fold_data_log_loss.run(clf, test_bunch)
 
